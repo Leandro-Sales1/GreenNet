57380cf81c52e1f61f56fa38e6b69a162b17ac10
Guilherme Duarte <GuilhermeCVD2711@outlook.com.br>
2023-07-30 21:09:07 -0300

    implementação da tela de games

---
+++ js/games.js
@@ -0,0 +1,29 @@
+const gabarito = ["1-a","2-d", "3-b", "4-d", "5-c"];
+function validaRespostas() {
+    let elementList = document.getElementsByClassName("form-check-input");
+    let countSuccess = 0;
+    for (let item of elementList) {
+        if (item.checked && gabarito.includes(item.id)){
+            item.parentElement.classList.add("option_success");
+            ++countSuccess;
+        }
+        else if (item.checked && !gabarito.includes(item.id)){
+            item.parentElement.classList.add("option_error");
+            document.getElementById(item.id[0] + "-menssagem").innerHTML = "Resposta correta seria: " + gabarito.find(x => x.startsWith(item.id[0]));
+        }
+        else{
+            item.parentElement.classList.add("disable_hover");
+        }
+    }
+    exibeMensagens(countSuccess);
+}
+
+function exibeMensagens(countSuccess){
+    let text = document.getElementById("text-modal");
+    if (countSuccess >= 3)
+        text.innerHTML = `Parabéns! Você acertou ${countSuccess} de 5 questões! Logo mais você recebera uma premiação em seu cartão de benefícios.`;
+    else
+        text.innerHTML = `Não foi dessa vez! Você acertou ${countSuccess} de 5 questões! Tente novamente amanhã.`;
+
+    document.getElementById("footer").innerHTML = "Volte amanhã para tentar ganhar mais prêmios!";
+}
