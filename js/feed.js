eccfdad373c7aa9be9dc454d701dba0cf5dc03ec
Marcus Sena <mens.eai20@uea.edu.br>
2023-09-09 06:34:50 -0400

    Adicionando funcionalidade ao botao de like

--- js/feed.js
+++ js/feed.js
@@ -1,22 +1,45 @@
 const estrela = document.querySelectorAll(".bi-star")
 console.log(estrela)
 
-estrela.forEach( (element) =>{
+estrela.forEach((element) => {
     element.addEventListener('click', () => {
-    if(element.classList.contains('bi-star')){
-        element.classList.remove('bi-star')
-        element.classList.add('bi-star-fill')}
-    else {
-        element.classList.remove('bi-star-fill')
-        element.classList.add('bi-star')}
+        if (element.classList.contains('bi-star')) {
+            element.classList.remove('bi-star')
+            element.classList.add('bi-star-fill')
+        }
+        else {
+            element.classList.remove('bi-star-fill')
+            element.classList.add('bi-star')
+        }
     }
-)})
+    )
+})
+const thumbs_up = document.querySelectorAll(".like-notfilled")
+console.log(thumbs_up)
 
-function funcionalidade_nao_disponivel(){
+thumbs_up.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (element.classList.contains('like-notfilled')) {
+            element.classList.remove('like-notfilled')
+            element.classList.add('like-filled')
+
+        }else{
+            element.classList.remove('like-filled')
+            element.classList.add('like-notfilled')
+        }
+    }
+    )
+})
+
+
+
+
+
+function funcionalidade_nao_disponivel() {
     alert("Funcionalidade não disponível")
 }
 
-function scrollToTopBtn(){
+function scrollToTopBtn() {
     window.scrollTo({
         top: 0,
         behavior: "smooth" // This makes the scroll behavior smooth
