3eb87a539e150c0d60895e45ce807537b2c8934d
Leandro-Sales1 <130797125+Leandro-Sales1@users.noreply.github.com>
2023-10-06 15:52:06 -0300

    Add files via upload

--- js/feed.js
+++ js/feed.js
@@ -1,5 +1,4 @@
 const estrela = document.querySelectorAll(".bi-star")
-console.log(estrela)
 
 estrela.forEach((element) => {
     element.addEventListener('click', () => {
@@ -15,7 +14,6 @@ estrela.forEach((element) => {
     )
 })
 const thumbs_up = document.querySelectorAll(".like-notfilled")
-console.log(thumbs_up)
 
 thumbs_up.forEach((element) => {
     element.addEventListener('click', () => {
@@ -23,7 +21,7 @@ thumbs_up.forEach((element) => {
             element.classList.remove('like-notfilled')
             element.classList.add('like-filled')
 
-        }else{
+        } else {
             element.classList.remove('like-filled')
             element.classList.add('like-notfilled')
         }
@@ -34,10 +32,110 @@ thumbs_up.forEach((element) => {
 
 
 
+const postPai = document.querySelector('#divPost')
+const postFilho = document.createElement('section')
+const formPost = document.querySelector('#novo_post')
+const primeiroPost = document.querySelector('#primeiroPost')
+const listaPost = []
 
-function funcionalidade_nao_disponivel() {
-    alert("Funcionalidade não disponível")
-}
+formPost.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log(event)
+    const titulo = event.target[0].value
+    const texto = event.target[1].value
+    const data = new Date
+    const video = event.target[2].value
+    const imagem = event.target[3].value
+    postFilho.innerHTML = `
+    <div class="card mx-3 mt-3" style="max-width: 700px;">
+    <div class="card-header" style="background-color:#E3FFE0">
+        <div class="d-flex flex-align-row align-items-center justify-content-start pb-2 mt-2">
+            <div>
+                <a href="../pages/profile_armando.html">
+                    <img src="../img/perfil_armando.jpg" alt="Foto de perfil do Armando"
+                        title="Foto de perfil do Armando" class="foto_perfil_post">
+                </a>
+            </div>
+            <div class="px-3">
+                <a href="../pages/profile_armando.html" style="color: black; text-decoration: none;">
+                    <div>Armando Silva</div>
+                </a>
+            </div>
+            <div class="px-3" style="margin-left: auto;">
+                ${data.getDay()}/${data.getMonth()+1} às ${data.getHours()}:${data.getMinutes()}
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body">
+        <div class="row py-1">
+            <div class="col-10 col-sm-11">
+                <h5 class="card-title">${titulo}</h5>
+            </div>
+            <div class="col-2 col-sm-1">
+                <i class="bi bi-star"></i>
+            </div>
+        </div>
+        <img src=${imagem||video} alt="" title="" class="card-img"
+        style="max-height: 200px; object-fit: cover;">
+
+        <p class="card-text">
+            ${texto}
+        </p>
+    </div>
+
+            <!--Botões-->
+            <div class="row py-2 " style="margin-top: -1rem;">
+                <!--Share-->
+                <div class="col d-flex justify-content-center botao-post">
+                    <div class="d-flex align-items-center justify-content-start px-3 py-3">
+                        <span class="material-symbols-outlined" alt="Compartilhar" title="Compartilhar">
+                            share
+                        </span>
+                        <span class="ps-2 d-none d-md-block">
+                            Compartilhar
+                        </span>
+
+                    </div>
+                </div>
+                <!--Comment-->
+                <div class="col d-flex justify-content-center botao-post">
+                    <div data-bs-toggle="modal" data-bs-target="#modal-1">
+                        <div class="d-flex align-items-center justify-content-start px-3 py-3">
+                            <span class="material-symbols-outlined" alt="Comentar" title="Comentar">
+                                mode_comment
+                            </span>
+                            <span class="ps-2 d-none d-md-block">
+                                Comentar
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Like-->
+                <div class="col d-flex justify-content-center botao-post">
+                    <div class="like-notfilled">
+                        <div class="d-flex align-items-center justify-content-start px-3 py-3">
+                            <span class="material-symbols-outlined" alt="Curtir" title="Curtir">
+                                thumb_up
+                            </span>
+                            <span class="ps-2 d-none d-md-block">
+                                Curtir
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    
+    listaPost.unshift(postFilho)
+    listaPost.forEach( item =>
+        postPai.appendChild(item))
+        
+    postPai.insertBefore(postFilho,primeiroPost) 
+})
+
+
 
 function scrollToTopBtn() {
     window.scrollTo({
