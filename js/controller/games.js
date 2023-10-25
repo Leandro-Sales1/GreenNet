72c3a3f57da6b4a594b9dc29c5f0a5f22772c3d6
Guilherme Duarte <GuilhermeCVD2711@outlook.com.br>
2023-10-24 23:17:04 -0300

    Impl Termo

--- js/controller/games.js
+++ js/controller/games.js
@@ -1,4 +1,4 @@
-// ------- QUIZ
+// --------------------- QUIZ --------------------- \\
 const gabarito = ["1-a", "2-d", "3-b", "4-d", "5-c"];
 function validaRespostas() {
     let elementList = document.getElementsByClassName("form-check-input");
@@ -29,7 +29,7 @@ function exibeMensagens(countSuccess) {
         text.innerHTML = `Nao foi dessa vez! Voce acertou ${countSuccess} de 5 questões! Tente novamente amanha.`;
 }
 
-// ------- JOGO DA FORCA
+// ----------------- JOGO DA FORCA ----------------- \\
 const _listaPalavraChave = [
     "sustentabilidade",
     "ecologia",
@@ -85,8 +85,6 @@ function clickBtnSortear() {
     iniciar();
 }
 
-
-
 // Funções auxiliares Btn Sortear
 function iniciar() {
     document.getElementById("tentativa").disabled = false;
@@ -99,7 +97,6 @@ function iniciar() {
 function selecionaPalavraChave() {
     _palavraChave = _listaPalavraChave[(Math.floor(Math.random() * _listaPalavraChave.length))].toLowerCase();
 }
-
 
 // Funções auxiliares Realiza Tentativa
 function realizaTentativa() {
@@ -139,16 +136,16 @@ function reduzRestanteTentativas() {
     document.getElementById("restante").innerHTML = --_restante;
 
 }
-function adicionaListErros(tentativa){
+function adicionaListErros(tentativa) {
     _erros += `${tentativa}-`;
-    document.getElementById("erros").innerHTML = _erros;    
+    document.getElementById("erros").innerHTML = _erros;
 }
 function exibeMensagensForca(message) {
     document.getElementById("text-modal-forca").innerHTML = message;
     document.getElementById("btnModalForca").click();
     reinicia();
 }
-function reinicia(){
+function reinicia() {
     _restante = 10;
     _palavraChave = "";
     _acertos = [];
@@ -163,7 +160,6 @@ function reinicia(){
     tentativa.placeholder = "";
 }
 
-
 // Funções gerais
 function criaLacunas() {
     let lacunas = "";
@@ -177,4 +173,246 @@ function criaLacunas() {
             lacunas += "__ ";
     }
     document.getElementById("lacunas").innerHTML = lacunas;
+}
+
+
+// --------------------- TERMO -------------------- \\
+var _palavrasEco = [
+    'ESTAR',
+    'JOVEM',
+    'AGUAS',
+    'FLORA',
+    'FAUNA',
+    'AMORA',
+    'LIXOS',
+    'SELVA',
+    'PAMPA',
+    'MELAO',
+    'SOLAR',
+    'VERDE',
+    'CICLO',
+    'CAMPO',
+    'POLEN',
+    'CHUVA',
+    'PRAIA',
+    'FUNGI',
+    'VITAL',
+    'UMIDO',
+    'VENTO',
+    'SERRA',
+    'REUSO',
+    'ARIDA',
+    'NOITE',
+    'SERES',
+    'LIRIO',
+    'VIVER',
+    'OASIS',
+    'VIVEU',
+    'PAZES',
+    'FOLIA',
+    'LIDER', 
+    'JOIAS',
+    'GRATO',
+    'CHAMA',
+    'HEROI',
+    'PRAÇA',
+    'DOCES',
+    'FESTA',
+    'GORDO',
+    'CESTA',
+    'CREME',
+    'ZEBRA',
+    'KIWIS',
+    'LIMAO',
+    'CANTO',
+];
+var _termo = [];
+var _restanteTermo = 6;
+var tentativa01 = document.getElementsByName('tentativa01');
+var tentativa02 = document.getElementsByName('tentativa02');
+var tentativa03 = document.getElementsByName('tentativa03');
+var tentativa04 = document.getElementsByName('tentativa04');
+var tentativa05 = document.getElementsByName('tentativa05');
+var tentativa06 = document.getElementsByName('tentativa06');
+
+document.getElementById("form").addEventListener('keydown', function (event) {
+    if (event.key == "Enter") {
+        validaTermo(retornaTentativaAtual(_restanteTermo));
+    }
+});
+let list = document.getElementById("form").getElementsByTagName("input");
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('keyup', function (event) {
+        if(event.key == 'Backspace' && i != 0)
+            list[(i - 1)].focus();
+        else if (event.key != "Enter")
+            list[(i + 1)].focus();
+    });
+}
+
+
+function initTermo() {
+    document.getElementById('btnInicarTermo').disabled = true;
+    selecionaTermo();
+    desabilitarTentativas(_restanteTermo, false);
+}
+function validaTermo(tentativaAtual) {
+    if (_restanteTermo == 0)
+        exibeMensagensTermo("Não foi dessa vez... tente novamente!");
+
+    let palavra = tolist(tentativaAtual);
+    if (palavra.length < 5){
+        alert("Só palavras com 5 letras");
+        return;
+    }
+
+    let acertos = [];
+    for (let i = 0; i < 5; i++) {
+        if (palavra[i] == _termo[i]) {
+            tentativaAtual.item(i).style.backgroundColor = '#E3FFE0';
+            acertos.push(i);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        if (_termo.includes(palavra[i]) && !acertos.includes(i)) {
+            tentativaAtual.item(i).style.backgroundColor = '#ffe9af';
+        }
+    }
+
+    if(palavra.toString() == _termo.toString()){
+        exibeMensagensTermo("Parabéns! Você ganhou!");
+        return;
+    }
+
+    desabilitarTentativas(_restanteTermo, true);
+    _restanteTermo--;
+    desabilitarTentativas(_restanteTermo, false);
+}
+
+
+function tolist(nodeList) {
+    let list = [];
+    for (const item of nodeList) {
+        if(item.value != "")
+            list.push(item.value.toUpperCase());
+    }
+    return list;
+}
+function selecionaTermo() {
+    _termo = _palavrasEco[(Math.floor(Math.random() * _palavrasEco.length))].split('');
+}
+function desabilitarTentativas(restantes, desabilitar, limpar) {
+    switch (restantes) {
+        case 6:
+            for (const item of tentativa01) {
+                item.disabled = desabilitar;
+
+                if(!desabilitar && item == tentativa01.item(0))
+                    item.focus();
+
+                if(limpar){
+                    item.value = "";
+                    item.style.backgroundColor = '';
+                }
+            }
+            break;
+        case 5:
+            for (const item of tentativa02) {
+                item.disabled = desabilitar;
+
+                if (!desabilitar && item == tentativa02.item(0))
+                    item.focus();
+
+                if (limpar) {
+                    item.value = "";
+                    item.style.backgroundColor = '';
+                }
+            }
+            break;
+        case 4:
+            for (const item of tentativa03) {
+                item.disabled = desabilitar;
+
+                if (!desabilitar && item == tentativa03.item(0))
+                    item.focus();
+                
+                if (limpar) {
+                    item.value = "";
+                    item.style.backgroundColor = '';
+                }
+            }
+            break;
+        case 3:
+            for (const item of tentativa04) {
+                item.disabled = desabilitar;
+
+                if (!desabilitar && item == tentativa04.item(0))
+                    item.focus();
+
+                if (limpar) {
+                    item.value = "";
+                    item.style.backgroundColor = '';
+                }
+            }
+            break;
+        case 2:
+            for (const item of tentativa05) {
+                item.disabled = desabilitar;
+
+                if (!desabilitar && item == tentativa05.item(0))
+                    item.focus();
+
+                if (limpar) {
+                    item.value = "";
+                    item.style.backgroundColor = '';
+                }
+            }
+            break;
+        case 1:
+            for (const item of tentativa06) {
+                item.disabled = desabilitar;
+
+                if (!desabilitar && item == tentativa06.item(0))
+                    item.focus();
+
+                if (limpar) {
+                    item.value = "";
+                    item.style.backgroundColor = '';
+                }
+            }
+            break;
+        default:
+            break;
+    }
+}
+function retornaTentativaAtual(restantes) {
+    switch (restantes) {
+        case 6:
+            return tentativa01;
+        case 5:
+            return tentativa02;
+        case 4:
+            return tentativa03;
+        case 3:
+            return tentativa04;
+        case 2:
+            return tentativa05;
+        case 1:
+            return tentativa06;
+        default:
+            break;
+    }
+}
+function reiniciaTermo(){
+    document.getElementById('btnInicarTermo').disabled = false;
+    _restanteTermo = 6;
+    _termo = [];
+    for (let i = 1; i <= 6; i++) 
+        desabilitarTentativas(i, true, true);
+}
+function exibeMensagensTermo(menssagem){
+    document.getElementById("text-modal-termo").innerHTML = menssagem;
+    document.getElementById("btnModalTermo").click();
+    reiniciaTermo();
 }
